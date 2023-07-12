2616312568e4d7ea167304fd5ec6fcd865794e7d
Kristof Szucs <a1040@MBP-de-IT-4.lan>
2023-07-12 23:59:33 +0200

    update

--- import_ES_data.py
+++ import_ES_data.py
@@ -1,39 +1,33 @@
 from elasticsearch import Elasticsearch
 import pandas as pd
 import warnings
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB
-
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-import re
-
-nltk.download('stopwords')
-nltk.download('punkt')
-print(stopwords.words('english'))
-
 warnings.filterwarnings('ignore')
 
-es = Elasticsearch(hosts = ["http://79.125.3.215:9200"])
 
-query = {
-    "query": {
-        "match_all": {}
-    },
-    "size": 1000  # Adjust batch size as per your requirements
-}
+def import_data_from_es(host, index):
+    
+    es = Elasticsearch(hosts = [host])
+    
+    query = {
+        "query": {
+            "match_all": {}
+        },
+        "size": 1000  # Adjust batch size as per your requirements
+    }
 
-scroll = es.search(index="asurion", body=query, scroll="5m")
-scroll_id = scroll["_scroll_id"]
+    scroll = es.search(index=index, body=query, scroll="5m")
+    scroll_id = scroll["_scroll_id"]
 
-hits = scroll["hits"]["hits"]
-data = [hit["_source"] for hit in hits]
-
-while len(hits) > 0:
-    scroll = es.scroll(scroll_id=scroll_id, scroll="5m")
     hits = scroll["hits"]["hits"]
-    data.extend([hit["_source"] for hit in hits])
+    data = [hit["_source"] for hit in hits]
 
-df = pd.DataFrame(data)
+    while len(hits) > 0:
+        scroll = es.scroll(scroll_id=scroll_id, scroll="5m")
+        hits = scroll["hits"]["hits"]
+        data.extend([hit["_source"] for hit in hits])
+
+    df = pd.DataFrame(data)
+    return df
+
+df = import_data_from_es(host = "http://79.125.3.215:9200", index = "asurion")
+print(df.shape)
